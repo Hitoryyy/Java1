2a2f11d6b8ff9d15fb49564a3c867bbe70efe600
Hitoryyy <hitory@yahoo.com>
2021-12-24 16:57:36 +0300

    Домашняя работа 1.

---
+++ src/main/java/home_work_1/HomeWorkApp.java
@@ -0,0 +1,48 @@
+package home_work_1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 7;
+        int b = 13;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 101;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Жёлтый");
+        } else if (value > 100) {
+            System.out.println("Зелёный");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 75;
+        int b = 33;
+        if (a > b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
