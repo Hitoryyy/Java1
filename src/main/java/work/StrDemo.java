684ec1f9a27ee6211aa61f95fb4a31fa23e0b0cd
Hitoryyy <95227982+Hitoryyy@users.noreply.github.com>
2022-02-02 16:34:58 +0300

    Delete StrDemo.java

--- src/main/java/work/StrDemo.java
+++
@@ -1,9 +0,0 @@
-package work;
-//Демонстрация управляющих последовательностей в символьных строках
-public class StrDemo {
-    public static void main(String[] args) {
-        System.out.println("Первая строка \nВторая строка");
-        System.out.println("A\tB\tC");
-        System.out.println("D\tE\tF");
-    }
-}
